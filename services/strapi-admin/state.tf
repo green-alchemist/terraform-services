3dabbfe12abd02eb2d4c63ed9c5beed06892ba5a
sigma-us <kyleconley2@gmail.com>
2023-01-18 19:34:15 -0800

    remote state source

--- services/strapi-admin/state.tf
+++ services/strapi-admin/state.tf
@@ -1,5 +1,4 @@
 module remote_state {
-  # source = "git@github.com:sigma-us/terraform-modules.git//AWS/modules/remote-state"
-  source = "../../../terraform-modules/AWS/modules/remote-state"
+  source = "git@github.com:sigma-us/terraform-modules.git//modules/remote-state"
   dynamodb_table_name = "strapi-admin-tf-lock"
 }
