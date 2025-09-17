da33684875e24b0dc5ea5bba754008af236b378d
green-alchemist <kyleconley2@gmail.com>
2025-09-17 00:41:40 -0700

    scale to zero, more customization

--- services/strapi-admin/database.tf
+++ services/strapi-admin/database.tf
@@ -1,8 +1,11 @@
 module "aurora_db" {
-  source             = "git@github.com:green-alchemist/terraform-modules.git//modules/aurora-serverless"
-  database_name      = "strapi"
-  master_username    = "strapiadmin"
-  master_password    = jsondecode(data.aws_secretsmanager_secret_version.db_password_version.secret_string)["db_password"]
-  subnet_ids         = [module.public_subnet.public_subnet_id]
-  security_group_ids = [module.aurora_security_group.security_group_id]
+  source                   = "git@github.com:green-alchemist/terraform-modules.git//modules/aurora-serverless"
+  database_name            = "strapi"
+  master_username          = "strapiadmin"
+  master_password          = jsondecode(data.aws_secretsmanager_secret_version.db_password_version.secret_string)["db_password"]
+  subnet_ids               = [module.public_subnet.public_subnet_id]
+  security_group_ids       = [module.aurora_security_group.security_group_id]
+  seconds_until_auto_pause = 600
+  max_capacity             = 1.0
+  min_capacity             = 0.0
 }
