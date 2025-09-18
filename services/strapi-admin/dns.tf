ce1b583ce37fc07d7c41f7eb557e59d40d0972af
green-alchemist <kyleconley2@gmail.com>
2025-09-18 00:39:45 -0700

    dns

--- services/strapi-admin/dns.tf
+++ services/strapi-admin/dns.tf
@@ -2,14 +2,14 @@ data "aws_route53_zone" "this" {
   name = "kconley.com" # Your root domain
 }
 
-module "dns_record" {
-  source = "git@github.com:green-alchemist/terraform-modules.git//modules/route53-record"
+# module "dns_record" {
+#   source = "git@github.com:green-alchemist/terraform-modules.git//modules/route53-record"
 
-  zone_id      = data.aws_route53_zone.this.zone_id
-  domain_name  = "kconley.com"
-  record_names = ["admin-${var.environment}"]
+#   zone_id      = data.aws_route53_zone.this.zone_id
+#   domain_name  = "kconley.com"
+#   record_names = ["admin-${var.environment}"]
 
-  # Point to the API Gateway's custom domain
-  alias_name    = module.api_gateway.api_endpoint # This will need to be updated to a custom domain
-  alias_zone_id = module.api_gateway.api_id       # This will need to be updated to a custom domain
-}
+#   # Point to the API Gateway's custom domain
+#   alias_name    = module.api_gateway.api_endpoint # This will need to be updated to a custom domain
+#   alias_zone_id = module.api_gateway.api_id       # This will need to be updated to a custom domain
+# }
