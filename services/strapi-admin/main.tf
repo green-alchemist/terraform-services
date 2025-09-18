e2e438149154f7fafda5dd61b75f01ee4c4f56f7
green-alchemist <kyleconley2@gmail.com>
2025-09-18 00:36:49 -0700

    no fargate or db test

--- services/strapi-admin/main.tf
+++ services/strapi-admin/main.tf
@@ -8,36 +8,36 @@ provider "aws" {
   region  = var.aws_region
 }
 
-module "strapi_fargate" {
-  source                      = "git@github.com:green-alchemist/terraform-modules.git//modules/fargate"
-  cluster_name                = "strapi-admin-cluster"
-  task_family                 = "strapi-admin-task"
-  service_name                = "strapi-admin-service"
-  container_name              = "strapi-admin"
-  ecr_repository_url          = module.strapi_ecrs.urls["strapi-admin-${var.environment}"]
-  ecs_task_execution_role_arn = module.ecs_task_execution_role.role_arn
-  subnet_ids                  = [module.public_subnet.public_subnet_id]
-  security_group_ids          = [module.strapi_security_group.security_group_id]
-  vpc_id                      = module.vpc.vpc_id
+# module "strapi_fargate" {
+#   source                      = "git@github.com:green-alchemist/terraform-modules.git//modules/fargate"
+#   cluster_name                = "strapi-admin-cluster"
+#   task_family                 = "strapi-admin-task"
+#   service_name                = "strapi-admin-service"
+#   container_name              = "strapi-admin"
+#   ecr_repository_url          = module.strapi_ecrs.urls["strapi-admin-${var.environment}"]
+#   ecs_task_execution_role_arn = module.ecs_task_execution_role.role_arn
+#   subnet_ids                  = [module.public_subnet.public_subnet_id]
+#   security_group_ids          = [module.strapi_security_group.security_group_id]
+#   vpc_id                      = module.vpc.vpc_id
 
-  # --- Enable Service Discovery ---
-  enable_service_discovery = true
-  private_dns_namespace    = "internal"
+#   # --- Enable Service Discovery ---
+#   enable_service_discovery = true
+#   private_dns_namespace    = "internal"
 
-  # --- Enable Scale-to-Zero ---
-  enable_autoscaling = true
-  min_tasks          = 0
-  max_tasks          = 1
+#   # --- Enable Scale-to-Zero ---
+#   enable_autoscaling = true
+#   min_tasks          = 0
+#   max_tasks          = 1
 
-  environment_variables = {
-    DATABASE_CLIENT   = "postgres"
-    DATABASE_HOST     = module.aurora_db.cluster_endpoint
-    DATABASE_PORT     = module.aurora_db.cluster_port
-    DATABASE_NAME     = module.aurora_db.database_name
-    DATABASE_USERNAME = "strapiadmin"
-    DATABASE_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.db_password_version.secret_string)["db_password"]
-  }
-}
+#   environment_variables = {
+#     DATABASE_CLIENT   = "postgres"
+#     DATABASE_HOST     = module.aurora_db.cluster_endpoint
+#     DATABASE_PORT     = module.aurora_db.cluster_port
+#     DATABASE_NAME     = module.aurora_db.database_name
+#     DATABASE_USERNAME = "strapiadmin"
+#     DATABASE_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.db_password_version.secret_string)["db_password"]
+#   }
+# }
 
 output "admin_url" {
   description = "The URL of the Strapi admin panel."
